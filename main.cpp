4ae7cdf939c3311379808fce7e9bf2962c4d05b1
Selisine <Aidenpike113@gmail.com>
2023-01-31 13:21:58 -0500

    Title Screen and Start of Game

--- main.cpp
+++ main.cpp
@@ -14,8 +14,12 @@ int main() {
     //Title Screen
     cout << "Welcome to Country Battle Plus: Remastered.\n";
     country.getNames();
-
+    cout << endl;
+    
     //Game
-    cout << country.returnName(1) << ", you have: ";
-    cout << country.returnName(2) << ", you have: ";
+    cout << country.returnName(1) << ", you have:\n";
+    country.returnStats(1);
+    
+    cout << country.returnName(2) << ", you have:\n";
+    country.returnStats(2);
 }
