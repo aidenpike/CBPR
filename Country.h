d5d70a9bb82894f9ae0e338bf2ed5fd4afe19ef0
Selisine <Aidenpike113@gmail.com>
2023-01-31 11:36:19 -0500

    Change all the stats from functions to vars

--- Country.h
+++ Country.h
@@ -15,31 +15,76 @@ class Country {
         void battleInitiation(int, int); //Second int is for battle outcome choice
         void pass(int);
     private:
+        //Player One Stats
         string pOneName;
+        int pOneTerritories;
+        int pOneArmies;
+        int pOneArmySkill;
+        int pOneArmyEndurance;
+        int pOneWeaponComplexity;
+        int pOnePassiveIncome;
+        //Player One Commander Stats
+        int pOneCmdrDmg;
+        int pOneCmdrDef;
+        int pOneCmdrHP;
+
+        //Player Two Stats
         string pTwoName;
-        //int = Player Number
-        int territoryCount(int);
-        int armyCount(int);
-        int returnMoney(int);
-        //char = Tech Tree
-        int pOneTechLevel(char);
-        int pTwoTechLevel(char);
+        int pTwoTerritories;
+        int pTwoArmies;
+        int pTwoArmySkill;
+        int pTwoArmyEndurance;
+        int pTwoWeaponComplexity;
+        int pTwoPassiveIncome;
+        //Player Two Commander Stats
+        int pTwoCmdrDmg;
+        int pTwoCmdrDef;
+        int pTwoCmdrHP;
 };
 
 //Implementation Section
 Country::Country(){ 
     //Public
-    expandTerritory(0);
-    upgradeTechnology(0, ' ');
-    battleInitiation(0, 0);
-    pass(0);
-    
     //Private
-    pOneName = "";
-    pTwoName = "";
-    territoryCount(0);
-    armyCount(0);
-    pOneTechLevel(0);
-    pTwoTechLevel(0);
-    returnMoney(0);
+        //Player One Stats
+        pOneName = "";
+        pOneTerritories = 0;
+        pOneArmies = 0;
+        pOneArmySkill = 0;
+        pOneArmyEndurance = 0;
+        pOneWeaponComplexity = 0;
+        pOnePassiveIncome = 0; 
+        //Player One Commander Stats
+        pOneCmdrDmg = 5;
+        pOneCmdrDef = 1;
+        pOneCmdrHP = 10;
+
+        //Player Two Stats
+        pTwoName = "";
+        pTwoTerritories = 0;
+        pTwoArmies = 0;
+        pTwoArmySkill = 0;
+        pTwoArmyEndurance = 0;
+        pTwoWeaponComplexity = 0;
+        pTwoPassiveIncome = 0; 
+        //Player Two Commander Stats
+        pTwoCmdrDmg = 5;
+        pTwoCmdrDef = 1;
+        pTwoCmdrHP = 10;
+}
+
+void expandTerritory(int player){
+
+}
+
+void upgradeTechnology(int player, char techBranch){
+    
+}
+
+void battleInitiation(int player){
+    
+}
+
+void pass(int player){
+    
 }
