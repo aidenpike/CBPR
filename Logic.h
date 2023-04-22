78b56f9987187c0d28b2743f4f4b7e424b38e014
Aiden Pike <61250126+aidenpike@users.noreply.github.com>
2023-04-21 22:10:20 -0400

    Add starter Logic functions

--- Logic.h
+++ Logic.h
@@ -23,17 +23,20 @@ class Logic {
             size_t playerArmies;
             size_t playerMoney;
         };
-
-        //Logic Functions
-        //Player
+        //Logic constructor for distributing values of Player
         Logic(std::string, size_t, size_t, size_t);
         static void retrieveNames(std::string, std::string);
 
         Player playerOne, playerTwo;
 
-        //PlayerTech
+        //Logic constructor for distributing values of PlayerTech
         Logic(size_t, size_t, size_t, size_t);
 
+        //Logic functions. Does all the math n stuff
+        void playerTurn(); //Runs all functions and dependency injects into the proper UI functions
+        size_t playerChoice(size_t); //Receives choice from UI and returns it for playerTurn()
+        size_t hasLost(); //Checks stats everytime its ran and returns the player number who has lost, or -1 by default
+        size_t returnDiceRoll(size_t, size_t); //Uses mersenne twister engine, takes amount of sides and amount of dice then returns sum of dice
 };
 
 
