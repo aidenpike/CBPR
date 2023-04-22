6ab9d6bb3cc4475454520ba39a3b9ae24ea1adb3
Aiden Pike <61250126+aidenpike@users.noreply.github.com>
2023-04-21 21:33:42 -0400

    Create Player Class in Logic Class

--- Logic.h
+++ Logic.h
@@ -3,7 +3,22 @@
 
 
 class Logic {
+    public:
+        class Player {
+        public:
+            Player();
+            static void retrieveNames(std::string, std::string);
 
+        private:
+            std::string playerName;
+            size_t playerTerritories;
+            size_t playerArmies;
+            size_t playerArmySkillLevel;
+            size_t playerArmyEnduranceLevel;
+            size_t playerWeaponComplexityLevel;
+            size_t playerPassiveIncomeLevel;
+            size_t playerMoney;
+        };
 };
 
 
