3bf7ed9e7cce297654af4a7bb98d2cb740e39eb6
Aiden Pike <61250126+aidenpike@users.noreply.github.com>
2023-04-23 18:46:46 -0400

    Add starter Logic functions

--- Logic.h
+++ Logic.h
@@ -24,13 +24,27 @@ class Logic {
             size_t playerMoney;
         };
         //Logic constructor for distributing values of Player
-        Logic(std::string, size_t, size_t, size_t);
-        static void retrieveNames(std::string, std::string);
+        Logic(size_t, size_t, size_t);
+        //Retrieve names method for putting into Logic constructor
+        static std::string retrieveNames(){
+            std::string name;
+
+            std::cout << "Player, enter your name: ";
+            std::cin >> name;
+
+            return name;
+        }
 
         Player playerOne, playerTwo;
 
         //Logic constructor for distributing values of PlayerTech
         Logic(size_t, size_t, size_t, size_t);
+
+        //Logic copies of player vars
+        std::string name;
+        size_t territories;
+        size_t armies;
+        size_t money;
 
         //Logic functions. Does all the math n stuff
         void playerTurn(); //Runs all functions and dependency injects into the proper UI functions
@@ -39,5 +53,24 @@ class Logic {
         size_t returnDiceRoll(size_t, size_t); //Uses mersenne twister engine, takes amount of sides and amount of dice then returns sum of dice
 };
 
+Logic::Player::Player(){
+    this->playerName = retrieveNames() + retrieveNames();
+    this->playerTerritories = 2;
+    this->playerArmies = 10;
+    this->playerMoney = 50'000;
 
+    Logic(playerTerritories, playerArmies, playerMoney);
+}
+
+Logic::Player::PlayerTech::PlayerTech(){
+    this->playerArmyEnduranceLevel = 1;
+    this->playerArmySkillLevel = 1;
+    this->playerPassiveIncomeLevel = 1;
+    this->playerWeaponComplexityLevel = 1;
+}
+Logic::Logic(size_t territories, size_t armies, size_t money){
+    this->territories = 2;
+    this->armies = 10;
+    this->money = 50'000;
+}
 #endif //CBPR_LOGIC_H
