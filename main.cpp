470adcb652fe6a955fc31a479802548454fb6c43
Selisine <Aidenpike113@gmail.com>
2023-09-13 08:41:04 -0400

    more testing

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include "game_logic.hpp"
 
 int main(){
+    //Maybe have game loop here and run turn counter through parameters
     game_logic game_logic;
     player_stats player_stats;
 
-    player_stats.assign_values();
+    game_logic.name_entry();
     game_logic.stat_rundown();
-    
+    game_logic.expand_territory();
+    game_logic.stat_rundown();
 }
