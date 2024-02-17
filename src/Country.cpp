47445ab8ce7fb74e24985b67574ac7f764f79391
Aiden Pike <aidenpike113@gmail.com>
2024-02-16 21:48:03 -0500

    Define the rest of the functions

--- src/Country.cpp
+++ src/Country.cpp
@@ -10,16 +10,17 @@ namespace cbpr {
         m_territories = territories;
         m_armies = armies;
     }
+
     void Country::alter_money(int value){
-        m_money -= value;
+        m_money += value;
     }
     void Country::increment_territories(){
         m_territories++;
-        alter_money(10'000);
+        alter_money(-10'000);
     }
     void Country::increment_armies(){
         m_armies++;
-        alter_money(8'000);
+        alter_money(-8'000);
     }
     int Country::return_random_int(int minimum, int maximum){
         std::random_device seed;  
@@ -28,8 +29,42 @@ namespace cbpr {
 
         return random_int(generation);
     }
+    void Country::initiate_attack(){
+
+    }   
+
+    int Country::get_money(){
+        return m_money;
+    }
+    int Country::get_territories(){
+        return m_territories;
+    } 
+    int Country::get_armies(){
+        return m_armies;
+    }
+
     Technology::Technology(int army_skill, int passive_income){
         m_army_skill = army_skill;
         m_passive_income = passive_income;
     }
+
+    void Technology::increment_army_skill(Country &o_country){
+        m_army_skill++;
+        o_country.alter_money(-1'000 - (10 * m_army_skill));
+    }
+    void Technology::increment_passive_income(Country &o_country){
+        m_passive_income++;
+        o_country.alter_money(-3'000 - (10 * m_passive_income));
+    }
+    void Technology::apply_passive_income(Country &o_country){
+        o_country.alter_money(m_passive_income);
+    }
+
+    int Technology::get_army_skill(){
+        return m_army_skill;
+    }
+    int Technology::get_passive_income(){
+        return m_passive_income;
+    }
+
 }; //namespace cbpr
